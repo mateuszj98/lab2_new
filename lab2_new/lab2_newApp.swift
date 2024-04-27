4441a21479310496a3f39b27ecbf68e4162321ae
Mateusz Jakubaszek <mateuszjakubaszek@gmail.com>
2024-04-27 11:16:06 +0200

    Initial Commit

---
+++ lab2_new/lab2_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2_newApp.swift
+//  lab2_new
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
